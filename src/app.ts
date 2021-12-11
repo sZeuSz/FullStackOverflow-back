99a6a3ab8e58efa2ec6831f5fb6815a6db05dd71
sZeuSz <zeus.k99@gmail.com>
2021-12-11 18:25:02 -0400

    refactor(all): add routes in app.ts 1_1

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,14 @@ import express from 'express';
 import * as questionsController from './controllers/questionController'
 import * as usersController from './controllers/userController';
 // import cors from "cors";
-import { Request, Response } from 'express';
 const app = express();
 // app.use(cors());
 app.use(express.json());
-app.post('/questions', questionsController.postQuestion);
-app.get('/questions/:id', questionsController.getQuestionById);
-app.post('/questions/:id', questionsController.postAnswer);
 
+app.post('/questions', questionsController.postQuestion);
+app.get('/questions', questionsController.getQuestions);
+app.post('/questions/:id', questionsController.postAnswer);
+app.get('/questions/:id', questionsController.getQuestionById);
+app.post('/users', usersController.userSignUp);
 
 export default app;
